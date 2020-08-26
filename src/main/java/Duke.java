9e8e5c613a8125fd79775f36c6c4a8bade59f2db
Li Bozhao <libozhao142857@hotmail.com>
2020-08-27 05:04:24 +0800

    Level 1. Greet, Echo, Exit
    skeletal code for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static String wrapMessageWithLines(String message){
+        String strLine = "____________________________________________________________\n";
+        return strLine + message + "\n" + strLine;
+    }
+
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        */
-        String greetings = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(greetings);
+        Scanner userInputGetter = new Scanner(System.in);
+        String greetings = " Hello! I'm Duke\n" + " What can I do for you?";
+        String bye = " Bye. Hope to see you again soon!";
+        System.out.print(wrapMessageWithLines(greetings));
+        String userInput = userInputGetter.nextLine();
+        while (!userInput.equals("bye")){
+            System.out.print(wrapMessageWithLines(userInput));
+            userInput = userInputGetter.nextLine();
+        }
+        System.out.print(wrapMessageWithLines(bye));
     }
 }
