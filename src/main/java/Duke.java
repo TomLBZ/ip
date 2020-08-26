cb35d4a837cd55bc6f596adf705b62f58f94f56d
Li Bozhao <libozhao142857@hotmail.com>
2020-08-27 05:51:34 +0800

    Level 2. Add, List
    modified the skeleton code so that it uses switch and responds to "list" command. It also adds text that is not a recognized command.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,50 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static String wrapMessageWithLines(String message){
-        String strLine = "____________________________________________________________\n";
-        return strLine + message + "\n" + strLine;
+    private static String addFormatLines(String[] messages, boolean isIndexed){
+        String strLine = "\t____________________________________________________________\n";
+        StringBuilder innerMessage = new StringBuilder();
+        for (int i = 0; i < messages.length; i++) {
+            if(isIndexed){
+                innerMessage.append("\t").append(i + 1).append(". ").append(messages[i]).append("\n");
+            }
+            else{
+                innerMessage.append("\t").append(messages[i]).append("\n");
+            }
+        }
+        return strLine + innerMessage + strLine;
+    }
+
+    private static String addFormatLines(String message){
+        String strLine = "\t____________________________________________________________\n";
+        return strLine + "\t" + message + "\n" + strLine;
     }
 
     public static void main(String[] args) {
         Scanner userInputGetter = new Scanner(System.in);
-        String greetings = " Hello! I'm Duke\n" + " What can I do for you?";
+        String[] greetings = new String[] {" Hello! I'm Duke", " What can I do for you?"};
         String bye = " Bye. Hope to see you again soon!";
-        System.out.print(wrapMessageWithLines(greetings));
-        String userInput = userInputGetter.nextLine();
-        while (!userInput.equals("bye")){
-            System.out.print(wrapMessageWithLines(userInput));
-            userInput = userInputGetter.nextLine();
+        System.out.print(addFormatLines(greetings, false));
+        ArrayList<String> addedCommands = new ArrayList<>();
+        boolean running = true;
+        while (running) {
+            String userInput = userInputGetter.nextLine();
+            switch (userInput) {
+            case "list":
+                String[] commands = new String[addedCommands.size()];
+                System.out.print(addFormatLines(addedCommands.toArray(commands), true));
+                break;
+            case "bye":
+                System.out.print(addFormatLines(bye));
+                running = false;
+                break;
+            default:
+                addedCommands.add(userInput);
+                System.out.print(addFormatLines("added: " + userInput));
+                break;
+            }
         }
-        System.out.print(wrapMessageWithLines(bye));
     }
 }
