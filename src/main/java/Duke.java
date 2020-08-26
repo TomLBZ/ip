dcd3863be95b9602c3c78c397cd97f7c45695e0c
Li Bozhao <libozhao142857@hotmail.com>
2020-08-27 06:21:59 +0800

    Level 3. Mark as Done
    Added done command, made the Task class to achieve that.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,12 @@ public class Duke {
     private static String addFormatLines(String[] messages, boolean isIndexed){
         String strLine = "\t____________________________________________________________\n";
         StringBuilder innerMessage = new StringBuilder();
+        if(isIndexed){
+            innerMessage.append("\tHere are the tasks in your list:\n");
+        }
         for (int i = 0; i < messages.length; i++) {
             if(isIndexed){
-                innerMessage.append("\t").append(i + 1).append(". ").append(messages[i]).append("\n");
+                innerMessage.append("\t").append(i + 1).append(".").append(messages[i]).append("\n");
             }
             else{
                 innerMessage.append("\t").append(messages[i]).append("\n");
@@ -27,22 +30,38 @@ public class Duke {
         String[] greetings = new String[] {" Hello! I'm Duke", " What can I do for you?"};
         String bye = " Bye. Hope to see you again soon!";
         System.out.print(addFormatLines(greetings, false));
-        ArrayList<String> addedCommands = new ArrayList<>();
+        ArrayList<Task> addedTasks = new ArrayList<>();
         boolean running = true;
         while (running) {
             String userInput = userInputGetter.nextLine();
             switch (userInput) {
             case "list":
-                String[] commands = new String[addedCommands.size()];
-                System.out.print(addFormatLines(addedCommands.toArray(commands), true));
+                String[] commands = new String[addedTasks.size()];
+                for(int i = 0; i < commands.length; i++){
+                    commands[i] = addedTasks.get(i).toString();
+                }
+                System.out.print(addFormatLines(commands, true));
                 break;
             case "bye":
                 System.out.print(addFormatLines(bye));
                 running = false;
                 break;
             default:
-                addedCommands.add(userInput);
-                System.out.print(addFormatLines("added: " + userInput));
+                if(userInput.contains("done")){
+                    int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                    if(index >= 0 && index < addedTasks.size()){
+                        addedTasks.get(index).markAsDone();
+                        System.out.print(addFormatLines("Nice! I've marked this task as done:\n\t\t" +
+                                addedTasks.get(index).toString()));
+                    }
+                    else{
+                        System.out.print(addFormatLines("Index specified is out of range."));
+                    }
+                }
+                else{
+                    addedTasks.add(new Task(userInput));
+                    System.out.print(addFormatLines("added: " + userInput));
+                }
                 break;
             }
         }
